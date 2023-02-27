c12fcf69024c2fae73366d0edbcaf488ccfd1f0c
Drorka~ <drorpa4@gmail.com>
2023-02-27 13:31:39 +0200

    design fixes

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -8,19 +8,12 @@
                 <RouterLink to="/">Home</RouterLink>
                 <RouterLink to="/contact">Contacts</RouterLink>
                 <RouterLink to="/stats">Statistics</RouterLink>
-                <!-- <RouterLink to="/about">About</RouterLink> -->
             </nav>
-            <!-- <div class="login-logout">
-            </div> -->
             <div class="user-info">
-                <!-- <span v-if="user">{{ user.name }}</span> -->
                 <RouterLink v-if="user" to="/user">{{ user.name }}</RouterLink>
-                <!-- <button class="btn-pinkish btn-logout" v-if="user">Logout</button> -->
-                <!-- <RouterLink v-if="user" @click="onLogout" to="/login">Logout</RouterLink> -->
                 <button class="btn-bluish" v-if="user" @click="onLogout">Logout</button>
                 <RouterLink v-if="!user" to="/login">Login</RouterLink>
             </div>
-            <!-- <span v-if="rate">1$ = {{ rate }}₿</span> -->
         </div>
     </header>
 </template>
@@ -32,11 +25,7 @@ import {userService} from '../services/user.service'
 export default {
     data() {
         return {
-            currRate: null,
         }
-    },
-    async created() {
-        this.currRate = await bitcoinService.getRate()
     },
     computed: {
         user() { return this.$store.getters.user }
